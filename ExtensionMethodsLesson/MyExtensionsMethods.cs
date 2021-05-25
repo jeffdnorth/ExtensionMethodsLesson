f122b173a7d2e14d2e2d37f97d76b0e06542a5f2
Jeff North <jnorth@tql.com>
2021-05-25 12:30:41 -0400

    Add project files.

---
+++ ExtensionMethodsLesson/MyExtensionsMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethodsLesson
+{
+    static class MyExtensionsMethods
+    {
+            public static string ToUpperCase(this string s)
+        {
+            return s.ToUpper();
+        }
+        public static void ToConsole(this string str)
+        {
+            Console.WriteLine($"The string is {str}");
+        }
+    }
+}
---
+++ ExtensionMethodsLesson/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtensionMethodsLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instead of writing out Console.Writeline
+            var abc = "abc";
+            abc.ToUpper().ToConsole();
+        }
+    }
+}
